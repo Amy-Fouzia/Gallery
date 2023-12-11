c613856effc9e16950f25ededf17f166128c811f
Tanvi Saini <ts4175@nyu.edu>
2023-12-11 15:30:34 -0500

    adding route files, throwing error
    "

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,13 +1,22 @@
+import Route from './components/Route'
+
+import './index.css';
+import artpage from './pages/Art';
 
 export default function App() {
   return (
     <div className="landingPage">
-      <header className="App-header">
+      <Route path='/'>
+      <h1 class="text-7xl font-delagothicone font-bold text-center p-5 hover:text-8xl">
         artVend
-      </header>
-      <img className="w-3/4 flex item" src={require('./assets/fortuneteller.jpeg')} />
-      {/* <img src={require('./assets/bubblegumMachine.jpeg')} />
-      <img src={require('./assets/toyvendor.jpeg')} /> */}
+      </h1>
+      </Route>
+      <div class="flex items-center justify-center">
+        <Route path="/art">
+          <img class="mainimg" src={require('./assets/toyvendor.jpeg')} />
+          <artpage />
+        </Route>
+      </div>
 
     </div>
   );
