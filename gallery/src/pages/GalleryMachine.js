6901b0e2a222b188c19fd5fc631cf4fa27ca975a
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-12 15:10:36 -0500

    gallery amchine

--- gallery/src/pages/GalleryMachine.js
+++ gallery/src/pages/GalleryMachine.js
@@ -1,37 +1,49 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Coins from '../components/Coins';
-
 const GalleryMachine = () => {
-  const [coinCount, setCoinCount] = useState(3);
-
-  const handleCoinButtonClick = () => {
-    // Decrease the coin count by 1
-    setCoinCount((prevCount) => Math.max(0, prevCount - 1));
+    const location = useLocation();
+    const navigate = useNavigate();
+  
+    // Parse the "coins" parameter from the URL
+    const initialCoinCount = parseInt(new URLSearchParams(location.search).get('coins'), 10) || 3;
+    const [coinCount, setCoinCount] = useState(initialCoinCount);
+  
+    const handleInsertCoinClick = () => {
+      if (coinCount > 0) {
+        setCoinCount((prevCount) => {
+          const updatedCount = prevCount - 1;
+          navigate(`/infoSlip?coins=${updatedCount}`);
+          return updatedCount;
+        });
+      }
+    };
+  
+    // Ensure the coin count is never negative
+    const updatedCoinCount = Math.max(coinCount, 0);
+  
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Gallery Machine Page</h1>
+  
+        {/* Display Coins component with correct coin count */}
+        <Coins coinCount={updatedCoinCount} />
+  
+        {/* Button to insert coin and navigate to InfoSlip */}
+        <button
+          onClick={handleInsertCoinClick}
+          className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+          disabled={updatedCoinCount === 0}
+        >
+          Insert Coin
+        </button>
+  
+        {/* Routes for GalleryMachine and InfoSlip */}
+        <Routes>
+          {/* Route for InfoSlip */}
+          <Route
+            path="/infoSlip"
+            element={<InfoSlip coinCount={updatedCoinCount} setCoinCount={setCoinCount} />}
+          />
+        </Routes>
+      </div>
+    );
   };
-
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Gallery Machine Page</h1>
-
-      {/* Display Coins component with coin count */}
-      <Coins coinCount={coinCount} />
-
-      {/* Button to decrease the coin count */}
-      <button
-        onClick={handleCoinButtonClick}
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
-        disabled={coinCount === 0}
-      >
-        Decrease Coin Count
-      </button>
-
-      {/* Link to navigate to Info Slip */}
-      <Link to="/infoSlip">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">Go to Info Slip</button>
-      </Link>
-    </div>
-  );
-};
-
-export default GalleryMachine;
+  
--- gallery/src/pages/InfoSlip.js
+++ gallery/src/pages/InfoSlip.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import GeorgeNelsonClock from '../components/GeorgeNelsonClock';
-import HermanMillerChair from '../components/HermanMillerChair';
-import RajaMatches from '../components/RajaMatches';
-
-const components = [GeorgeNelsonClock, HermanMillerChair, RajaMatches];
-
-const getRandomComponent = () => {
-  const randomIndex = Math.floor(Math.random() * components.length);
-  return components[randomIndex];
-};
-
 const InfoSlip = () => {
-  const RandomComponent = getRandomComponent();
-
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Entrance Page</h1>
-      <p>info slip</p>
-      <RandomComponent />
-    </div>
-  );
-};
-
-export default InfoSlip;
+    const RandomComponent = getRandomComponent();
+    const navigate = useNavigate();
+    const location = useLocation();
+  
+    const handleCloseClick = () => {
+      const coinCount = location?.search ? parseInt(new URLSearchParams(location.search).get('coins'), 10) : 0;
+  
+      // Navigate back to GalleryMachine with the updated coin count
+      navigate(`/galleryMachine?coins=${coinCount}`);
+    };
+  
+    // Get the updated coin count from the query parameter
+    const updatedCoinCount = location?.search ? parseInt(new URLSearchParams(location.search).get('coins'), 10) : 0;
+  
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Entrance Page</h1>
+        <p>info slip</p>
+        <RandomComponent />
+  
+        {/* Close button to navigate back to GalleryMachine */}
+        <button onClick={handleCloseClick} className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
+          Close
+        </button>
+      </div>
+    );
+  };
+  const InfoSlip = () => {
+    const RandomComponent = getRandomComponent();
+    const navigate = useNavigate();
+    const location = useLocation();
+  
+    const handleCloseClick = () => {
+      const coinCount = location?.search ? parseInt(new URLSearchParams(location.search).get('coins'), 10) : 0;
+  
+      // Navigate back to GalleryMachine with the updated coin count
+      navigate(`/galleryMachine?coins=${coinCount}`);
+    };
+  
+    // Get the updated coin count from the query parameter
+    const updatedCoinCount = location?.search ? parseInt(new URLSearchParams(location.search).get('coins'), 10) : 0;
+  
+    return (
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Entrance Page</h1>
+        <p>info slip</p>
+        <RandomComponent />
+  
+        {/* Close button to navigate back to GalleryMachine */}
+        <button onClick={handleCloseClick} className="bg-blue-500 text-white px-4 py-2 rounded mt-4">
+          Close
+        </button>
+      </div>
+    );
+  };
+    
