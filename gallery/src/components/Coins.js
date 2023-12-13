9e5af0e98bf6c11b808b106aff941657fa8df7e3
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-13 11:21:10 -0500

    last in tweak

--- gallery/src/components/Coins.js
+++ gallery/src/components/Coins.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import coinImage from '../assets/coin.png'; // Update the path accordingly
+import coinImage from '../assets/coin.png';
 
 const Coins = ({ coinCount }) => {
   return (
     <div>
       <div className="flex-row" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50px' }}>
-        {/* Display the coins based on the current coin count */}
         {Array.from({ length: coinCount }).map((_, index) => (
           <img key={index} src={coinImage} alt={`Coin ${index + 1}`} className="w-16 h-16 mr-2" />
         ))}
