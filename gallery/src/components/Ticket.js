84950156a4118c7901b0cc9f3e13713e5f2517d7
Amy-Fouzia <70932386+Amy-Fouzia@users.noreply.github.com>
2023-12-13 11:15:30 -0500

    last in tweak

--- gallery/src/components/Ticket.js
+++ gallery/src/components/Ticket.js
@@ -3,23 +3,21 @@ import ticket from '../assets/ticket.png';
 import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GalleryMachine from '../pages/GalleryMachine';
 
-
 const Ticket = () => {
-
   return (
-    <div>
-    <div class="flex flex-col justify-center">
-        <img class="px-60" src={ticket} />
-        <h1 class="font-sans p-7"> 
-        <Link to="/galleryMachine?coins=3" class="font-lato text-dark-brown" style={{ textDecoration: 'none', color: 'black' }}>
+    <div className="flex flex-col items-center justify-center">
+      <Link to="/galleryMachine?coins=3" className="font-lato text-dark-brown" style={{ textDecoration: 'none', color: 'black' }}>
+        <img className="px-60" src={ticket} alt="Ticket" />
+      </Link>
+      <h1 className="font-sans p-7">
+        <Link to="/galleryMachine?coins=3" className="font-lato text-dark-brown" style={{ textDecoration: 'none', color: 'black' }}>
           Collect ticket and Enter
         </Link>
-        </h1>
-    </div>
+      </h1>
 
-    <Routes>
-          <Route path="/galleryMachine?coins=3" element={<GalleryMachine />} />
-        </Routes>
+      <Routes>
+        <Route path="/galleryMachine?coins=3" element={<GalleryMachine />} />
+      </Routes>
     </div>
   );
 };
